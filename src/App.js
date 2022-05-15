f708f5baef67b2f946779186a0593030684c5513
hargun0360 <hargunsingh0720@gmail.com>
2022-05-15 17:41:46 +0530

    Add Login Page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import Signup from './components/auth/SignUp/SignUp';
+import Login from './components/auth/Login/Login';
 function App() {
   return ( <>
        <Routes>
         <Route exact path="/signup" element={<Signup />} />
+        <Route exact path="/login" element={<Login />} />
       </Routes>
     </>
   );
