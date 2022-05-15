be065e1fa30766cc2623a61aeac33d63c93de63f
saksham-tiwari <lucky121saty@gmail.com>
2022-05-15 12:54:26 +0530

    added MUI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
+import Home from './components/layout/Home';
 
 function App() {
   return (
     <>
-      Hello KIET
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+      </Routes>
     </>
   );
 }
