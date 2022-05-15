7c20db7a87ac70c3b5174a6355aec43d1686a1d0
hargun0360 <hargunsingh0720@gmail.com>
2022-05-15 18:23:12 +0530

    Add OTP Page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route, useLocation, useNavigate } from 'react-router-dom'
 import Signup from './components/auth/SignUp/SignUp';
 import Login from './components/auth/Login/Login';
+import Otp from './components/auth/OTP/Otp';
 function App() {
   return ( <>
        <Routes>
         <Route exact path="/signup" element={<Signup />} />
         <Route exact path="/login" element={<Login />} />
+        <Route exact path="/otp" element={<Otp />} />
       </Routes>
     </>
   );
